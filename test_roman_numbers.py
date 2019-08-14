aa3c725d963053bea9aca0e0d15759f5bc1f793d
santigg19 <graffigna.santi@gmail.com>
2019-08-14 12:40:20 -0300

    updated 1.3

--- test_roman_numbers.py
+++ test_roman_numbers.py
@@ -1,11 +1,12 @@
 import unittest
 from roman_numbers import roman_to_decimal
 
+
 class TestTomanNumbers(unittest.TestCase):
 
     def test_roman_I_to_decimal(self):
-        decimal_number = roman_to_decimal ('I')
-        self.assertEqual(decimal_number)
+        decimal_number = roman_to_decimal('I')
+        self.assertEqual(decimal_number, 1)
 
 
 if __name__ == '__main__':
